b4926378148892c2b415a20de829ac108dedba0e
altor <arthur.dazemar@free.fr>
2015-06-09 11:21:13 +0000

    resolve bug

--- src/json_decoder.erl
+++ src/json_decoder.erl
@@ -8,7 +8,6 @@
 -vsn(1).
 
 
-
 %% enlève les espace et le : entre le nom d'un champ et sa valeur
 purify(String) -> purify(String, false).
 purify([32 | T], Bool) -> purify(T, Bool);
@@ -16,6 +15,7 @@ purify([$: | _], true) -> throw('parse_error : too much :'); %% il y a un : en t
 purify([$: | T], false) -> purify(T, true);
 purify(String, true) -> String;
 purify(_, false) -> throw('parse_error : missing : '). %%le : manque pour séparer le champ de la valeur
+
 
 %% extrait une chaine de caractère json 
 -spec extract_string(string()) -> {string(), string()}.
@@ -27,21 +27,17 @@ extract_string(String) ->
     case purify(String) of
 	[$\\, $" | S] -> extract_string(S, "");
 	Other ->
-	    io:format("~w~n",[Other]),
 	    throw("parse_error : the string " ++ Other ++ "dont't begin with \\\"")
     end.
 
 
 -spec extract_string(string(), string()) -> {string(), string()}.
-extract_string([C], Acc) ->
-    if 
-	(C =:= $")  -> {lists:reverse(Acc), []};
-	true ->  throw('parse_error : end of stream before object ending')
-    end;
+extract_string([], _) ->
+    throw('parse_error : end of stream before object ending');
 
 extract_string([C | T], Acc) ->
-    case C of
-	$, ->
+    if 
+	(C =:= $,) or (C =:= $}) ->
 	    Acc_striped = string:strip(Acc, left),
 	    case Acc_striped of
 		[$", $\\, $\\ | _] -> extract_string(T, [C | Acc_striped]);
@@ -49,76 +45,105 @@ extract_string([C | T], Acc) ->
 		[$", $\\ | Acc_tail] -> {lists:reverse(Acc_tail), T};
 		_ -> extract_string(T, [C | Acc])
 	    end;
-	_ -> extract_string(T, [C | Acc])
+	true -> extract_string(T, [C | Acc])
     end.
 
+%%extrait une valeur json d'une chaine
+%%AF lancer une exeption quand un espace est entre 2 valeur ex : {champ:  val val}
+%% extract_value([ C | T]) when (C =:= 32) or (C =:= $:) ->
+%%     extract_value(T);
 extract_value(String) ->
     extract_value(purify(String),"").
 
-extract_value([$, | T], Acc) -> 
+extract_value([C | T], Acc) when (C =:= $,) or (C =:= $})-> 
     {lists:reverse(string:strip(Acc, left)), T};
 extract_value([C | T], Acc) ->
     extract_value(T, [C | Acc]).
 
-
+simple_echaped([$\\ ]) -> true;
 simple_echaped([$\\ , C |_]) -> C =/= $\\;
 simple_echaped(_) -> false.
 
-    
-extract_list(String) ->
-    extract_list(purify(String), "", false, 0).
 
-extract_list([$" | T], Acc,  false, N) -> 
+extract_object(String, Separator_end, Separator_begin) ->
+    extract_object(purify(String), "", false, 0, Separator_end, Separator_begin).
+
+extract_object([$" | T], Acc,  false, N, Separator_end, Separator_begin) -> 
     case simple_echaped(Acc) of  
-	true -> extract_list(T, [$" | Acc], true, N);
+	true -> extract_object(T, [$" | Acc], true, N, Separator_end, Separator_begin);
 	false -> throw("parse error : too much \\")
     end;
-extract_list([$" | T], Acc, true, N) -> 
+extract_object([$" | T], Acc, true, N, Separator_end, Separator_begin) -> 
 	case simple_echaped(Acc) of
-	    true -> extract_list(T, [$" | Acc],  false, N);
-	    false -> extract_list(T, [$" | Acc], true, N)
+	    true -> extract_object(T, [$" | Acc],  false, N, Separator_end, Separator_begin);
+	    false -> extract_object(T, [$" | Acc], true, N, Separator_end, Separator_begin)
     end;
-extract_list([C | T], Acc, true, N) ->
-    extract_list(T, [C | Acc], true, N);
-extract_list([C | T], Acc, false, N_bracket) when C =:= $[->
-    extract_list(T, [C | Acc], false, N_bracket + 1);
-extract_list([C | T], Acc, false, 1) when C =:= $]->
+extract_object([C | T], Acc, true, N, Separator_end, Separator_begin) ->
+    extract_object(T, [C | Acc], true, N, Separator_end, Separator_begin);
+extract_object([C | T], Acc, false, N_separator, Separator_end, Separator_begin) when C =:= Separator_begin->
+    extract_object(T, [C | Acc], false, N_separator + 1, Separator_end, Separator_begin);
+extract_object([C | T], Acc, false, 1, Separator_end, _) when C =:= Separator_end->
     T_striped = string:strip(T, left),
     case T_striped of
 	[$, | T2 ] -> {lists:reverse([C|Acc]), T2};
+	[$} | T2 ] -> {lists:reverse([C|Acc]), T2};
 	_ -> throw("parse error : missing ,")
     end;
-extract_list([C | T], Acc, false, N_bracket) when C =:= $]->
-    extract_list(T, [C | Acc], false, N_bracket - 1);
-extract_list([C | T], Acc, In_string, N) ->
-    extract_list(T, [C | Acc], In_string, N).
+extract_object([C | T], Acc, false, N_separator, Separator_end, Separator_begin) when C =:= Separator_end->
+    extract_object(T, [C | Acc], false, N_separator - 1, Separator_end, Separator_begin);
+extract_object([C | T], Acc, In_string, N, Separator_end, Separator_begin) ->
+    extract_object(T, [C | Acc], In_string, N, Separator_end, Separator_begin).
+
 
 %%renvoi la liste des valeurs extraites
 -spec extract_list_value(string()) -> [string()].
 extract_list_value(String) ->
     [_ | T] = String,
-    extract_list_value(T, "", [], false, 1).
+    extract_list_value(T, {"", []}, false, 0, 1).
 
-extract_list_value([$" | T], Acc_value, Acc_list, false, N) ->
-    case simple_echaped(Acc_value) of  
-	true -> extract_list_value(T, [$" | Acc_value],Acc_list, true, N);
+extract_list_value([$" | T], {Acc_value, Acc_list}, false, N_accolade, N) ->
+    case simple_echaped(Acc_value) of 
+	true -> extract_list_value(T, {[$" | Acc_value],Acc_list}, true, N_accolade, N);
 	false -> throw("parse error : too much \\")
     end;
-extract_list_value([$" | T], Acc_value, Acc_list, true, N) -> 
-	case simple_echaped(Acc_value) of
-	    true -> extract_list_value(T, [$" | Acc_value], Acc_list,  false, N);
-	    false -> extract_list_value(T, [$" | Acc_value], Acc_list, true, N)
+extract_list_value([$" | T], {Acc_value, Acc_list}, true, N_accolade, N) -> 
+    case simple_echaped(Acc_value) of
+	true -> Acc = { [$" | Acc_value], Acc_list},
+		extract_list_value(T,Acc , false, N_accolade, N);
+	false -> Acc = { [$" | Acc_value], Acc_list},
+		 extract_list_value(T,Acc, true, N_accolade, N)
     end;
-extract_list_value([C | _], Acc_value, Acc_list, false, 1) when (C =:= $]) ->
+extract_list_value([C | _], Acc, false, _, 1) when (C =:= $]) ->
+    {Acc_value, Acc_list} = Acc,
     lists:reverse([string:strip(lists:reverse(Acc_value), both)  | Acc_list]);
-extract_list_value([C | T], Acc_value, Acc_list, false, N_bracket) when C =:= $[->
-    extract_list_value(T, [C | Acc_value], Acc_list, false, N_bracket + 1);
-extract_list_value([C | T], Acc_value, Acc_list, false, N_bracket) when (C =:= $]) ->
-    extract_list_value(T, [C | Acc_value], Acc_list, false, N_bracket - 1);
-extract_list_value([C | T], Acc_value, Acc_list, false, 1) when (C =:= $,) ->
-    extract_list_value(T, "", [string:strip(lists:reverse(Acc_value), both) | Acc_list], false, 1);
-extract_list_value([C | T], Acc_value, Acc_list, In_string, N_bracklet) ->
-    extract_list_value(T, [C | Acc_value], Acc_list, In_string, N_bracklet).
+extract_list_value([C | T], Acc, false, N_accolade, N_bracket) when C =:= $[->
+    {Acc_value, Acc_list} = Acc,
+    Acc2 = {[C | Acc_value], Acc_list},
+    extract_list_value(T, Acc2, false, N_accolade, N_bracket + 1);
+extract_list_value([C | T],Acc , false, N_accolade, N_bracket) when (C =:= $]) ->
+    {Acc_value, Acc_list} = Acc,
+    Acc2 = {[C | Acc_value], Acc_list},
+    extract_list_value(T, Acc2, false, N_accolade, N_bracket - 1);
+
+extract_list_value([C | T], Acc, false, N_accolade, N_bracket) when C =:= ${->
+    {Acc_value, Acc_list} = Acc,
+    Acc2 = {[C | Acc_value], Acc_list},
+    extract_list_value(T, Acc2, false, N_accolade + 1, N_bracket);
+extract_list_value([C | T],Acc , false, N_accolade, N_bracket) when (C =:= $}) ->
+    {Acc_value, Acc_list} = Acc,
+    Acc2 = {[C | Acc_value], Acc_list},
+    extract_list_value(T, Acc2, false, N_accolade - 1, N_bracket);
+
+extract_list_value([C | T], Acc, false, 0, 1) when (C =:= $,) ->
+    {Acc_value, Acc_list} = Acc,
+    Value = string:strip(lists:reverse(Acc_value), both),
+    Acc2 = { "", [ Value | Acc_list]},
+    extract_list_value(T,Acc2, false, 0, 1);
+extract_list_value([C | T], Acc, In_string, N_accolade, N_bracklet) ->
+    {Acc_value, Acc_list} = Acc,
+    Acc2 = {[C | Acc_value], Acc_list},
+    extract_list_value(T, Acc2, In_string, N_accolade, N_bracklet).
+
 
 %% convertie une liste json en liste erlang
 parse_pure_list(String, Type) ->
@@ -132,7 +157,7 @@ parse_pure_list(String, Type) ->
 
 parse_impure_list(String, Types) ->
     case (String) of
-	[$[ | List_string] ->  
+	[$[ | _] ->  
 	    P = fun ({Value, Type}, Acc) -> [convert_value(Value, Type) | Acc] end,
 	    Value_list = extract_list_value(String),
 	    Info_list = lists:zip(Value_list, Types),
@@ -169,12 +194,16 @@ convert_value_aux(Value, {pure_list, Type}) ->
     {ok, parse_pure_list(Value, Type)};
 convert_value_aux(Value, {impure_list, Type}) ->
     {ok, parse_impure_list(Value, Type)};
+convert_value_aux(Value, {object, F}) ->
+    {ok, F(Value)};
 convert_value_aux(Value, float) ->
     coers:to_float(Value).
 
 %% -spec parse_object(string(), #{string() => json_type()}, #{string() => any()}) -> [{string(), any()}].
+parse_object(String, Field_info) ->
+    parse_object(String, Field_info, maps:new()).
 
-parse_object([], _, _) -> throw(parse_error);
+parse_object([], _, Acc) -> Acc;
 
 parse_object([C | T], Field_info, Acc) ->
     case C of
@@ -191,13 +220,17 @@ parse_object([C | T], Field_info, Acc) ->
 			   error -> throw(wrong_field_name);
 			   {ok, string} ->
 			       {Value, T4} = extract_string(T3),
-			       parse_object(T4, Field_info, maps:put(Field_name, Value, Acc));
+			       Value2 = maps:put(Field_name, Value, Acc),
+			       parse_object(T4, Field_info, Value2);
 			   {ok, {pure_list, Type}} ->
-			       {Value, T4} = extract_list(T3),
+			       {Value, T4} = extract_object(T3, $], $[),
 			       parse_object(T4, Field_info, maps:put(Field_name, convert_value(Value, {pure_list, Type}), Acc));
 			   {ok, {impure_list, Type}} ->
-			       {Value, T4} = extract_list(T3),
+			       {Value, T4} = extract_object(T3, $], $[),
 			       parse_object(T4, Field_info, maps:put(Field_name, convert_value(Value, {impure_list, Type}), Acc));
+			   {ok, {object, F}} ->
+			       {Value, T4} = extract_object(T3, $}, ${),
+			       parse_object(T4, Field_info, maps:put(Field_name, F(Value), Acc));
 			   {ok, Type} -> 
 			       {Value, T4} = extract_value(T3),
 			       parse_object(T4, Field_info, maps:put(Field_name, convert_value(Value, Type), Acc))
@@ -217,18 +250,11 @@ map_to_record(Map, Fields, Record_name) ->
 
 json_to_record(String, Type_list, Record_name, Fields) ->
     Map = maps:from_list(lists:zip(Fields, Type_list)),
-    Result = parse_object(String, Map, maps:new()),
+    Result= parse_object(String, Map),
     map_to_record(Result, Fields, Record_name).
     
 
-
-%% @doc generate a function which convert a json string into an erlang record
-%% @spec gen_decoder(Type_list:: [json_type()], Fields::[string()], Record_name::atom()) -> fun ((string()) -> tuple())
-%% gen_encoder(Type_list, Fields, Record_name)
--spec gen_decoder(Type_list:: [json_type()], Fields::[string()], Record_name::atom()) -> fun ((string()) -> tuple()).
 gen_decoder(Type_list, Fields, Record_name) ->
     fun(String) ->
 	    json_to_record(String, Type_list, Record_name, Fields)
     end.
-
-
