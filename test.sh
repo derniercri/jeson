6ad77ebf255e24b874c42d5d1f4a0bd19c97689c
altor <arthur.dazemar@free.fr>
2015-06-11 21:48:25 +0000

    update test.sh

--- test.sh
+++ test.sh
@@ -1,24 +1,17 @@
 #!/bin/bash
 
-test_module(){
-    echo "=================="
-    echo "Testing modules : $1"
-    echo "=================="
-    erl -pa ebin -noshell -s $1 test -s init stop
-}
+for i in $*
+do
+    if ! test -e ebin/$i.beam
+    then
+	echo "error : file ebin/$i.beam doesn't exist"
+    else
+	echo "=================="
+	echo "Testing module : $1"
+	echo "=================="
+	erl -pa ebin -noshell -s $i test -s init stop
+    fi
+done
 
-test_list(){
-    for i in $1
-    do
-	if ! test -e ebin/$i.beam
-	then
-	    echo "error : file ebin/$i.beam don't exist"
-	else
-	    test_module $i
-	fi
-    done
-}
-
-test_list $*
 
 
